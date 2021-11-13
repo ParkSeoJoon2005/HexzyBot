2489c9d14da687246c8c25f85d00af3aaf846028
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-11-13 14:33:00 +0530

    Update musicplayer.py

--- HexzyBot/modules/musicplayer.py
+++ HexzyBot/modules/musicplayer.py
@@ -1,10 +1,12 @@
-__mod_name__ = "⚡️Music-Play⚡️"
+__mod_name__ = "🎧VC"
 
 __help__ = """
-=>> *Song Playing* 🎧 
+◉*Playing* 🎧 
 ❍ /play <song name> - play song you requested
 ❍ /dplay <song name> - play song you requested via deezer
 ❍ /splay <song name> - play song you requested via jio saavn
+❍ /vplay <song name> 
+          <URL>         -to play video
 ❍ /playlist - Show now playing list
 ❍ /current - Show now playing
 ❍ /song <song name> - download songs you want quickly
@@ -12,7 +14,7 @@ __help__ = """
 ❍ /deezer <song name> - download songs you want quickly via deezer
 ❍ /saavn <song name> - download songs you want quickly via saavn
 ❍ /video <song name> - download videos you want quickly
-=>> *Admins only*
+◉*Admins Only*
 ❍ /player - open music player settings panel
 ❍ /pause - pause song play
 ❍ /resume - resume song play
@@ -20,7 +22,7 @@ __help__ = """
 ❍ /end - stop music play
 ❍ /userbotjoin - invite assistant to your chat
 ❍ /reload - Refresh admin list
-=>>   *Use*
-1) Add @HexzyBot in your group to admin with all permission
-2) Add Assistant I'd to your group @HexzyAssistant
+◉*Use*
+1) Add @AuroraSKBot in your group to admin with all permission
+2) Add Assistant I'd to your group @Xzavier_16
 """
