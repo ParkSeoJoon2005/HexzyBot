3993eeeb239d8a86b57258f849e656c4f3f5d11a
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-11-20 21:56:42 +0530

    Update disable.py

--- HexzyBot/modules/disable.py
+++ HexzyBot/modules/disable.py
@@ -339,18 +339,20 @@ if is_module_loaded(FILENAME):
 
     __help__ = """
     ❍ /cmds*:* check the current status of disabled commands
-
     *Admins only:*
     ❍ /enable <cmd name>*:* enable that command
-    ❍ /disable <cmd name>*:* disable that command
-    ❍ /enablemodule <module name>*:* enable all commands in that module
+    ❍ /disable <cmd name>*:* disable that command     ❍ /enablemodule <module name>*:* enable all commands in that module
     ❍ /disablemodule <module name>*:* disable all commands in that module
     ❍ /listcmds*:* list all possible toggleable commands
-   """
-    
-   __mod_name__ = "Disablings✖"
+
+    """
+
+    __mod_name__ = "⚡️Disable⚡️"
 
 else:
+
     DisableAbleCommandHandler = CommandHandler
+
     DisableAbleRegexHandler = RegexHandler
+
     DisableAbleMessageHandler = MessageHandler
